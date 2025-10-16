76ea8b8c634f3e9033d96ae7c451b839aadc2c7f
farouk <bouzizfarouk3@gmail.com>
2025-10-16 15:31:03 +0100

    Added marquee Animation effect to partners section

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,22 +1,23 @@
 import * as Brands from "@/assets/svg/Partners";
+import Marquee from "react-fast-marquee";
 
 const Logos = () => {
   return (
     <section className="section !px-0">
-      <h2 className="text-big mb-5 text-center">
-        Trusted by world's top brands
+      <h2 className="text-big mb-5 text-center lg:mb-10">
+        Trusted by world&apos;s top brands
       </h2>
-      <div className="relative">
-        <div className="flex items-center justify-center gap-8 overflow-hidden lg:gap-12">
-          {Object.entries(Brands).map(([name, Icon]) => (
-            <Icon key={name} className="brand" />
-          ))}
-        </div>
-        <div className="absolute top-0 flex h-full w-full items-center justify-between">
-          <div className="linear-bg h-full w-[70]"></div>
-          <div className="linear-bg h-full w-[70] rotate-180"></div>
-        </div>
-      </div>
+      <Marquee
+        pauseOnClick
+        loop={0}
+        speed={150}
+        gradient
+        gradientColor="var(--background)"
+      >
+        {Object.entries(Brands).map(([name, Icon]) => (
+          <Icon key={name} className="brand mx-5" />
+        ))}
+      </Marquee>
     </section>
   );
 };
