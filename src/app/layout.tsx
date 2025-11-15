883b1d5f3baee403afd84bef4d6e1f923830a898
farouk <bouzizfarouk3@gmail.com>
2025-11-15 20:48:19 +0100

    Removed extra GTM script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -147,26 +147,7 @@ export default async function RootLayout({
       className={isLight ? "light" : isLight === false ? "dark" : ""}
     >
       <body className={`${manrope.variable} ${knockbold.variable} antialiased`}>
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K8GKQ35X"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
         {children}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-K8GKQ35X');`,
-          }}
-        />
         <Script
           id="jsonLd"
           strategy="afterInteractive"
